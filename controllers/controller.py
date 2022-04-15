0aca568b0cc7813ca5bad65630fc5d3eff979cfa
emma-dougherty <doughertyemma@hotmail.com>
2022-04-15 20:56:03 +0100

    second commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -12,4 +12,4 @@ def play(player1_choice, player2_choice):
     player1 = Player("Player 1", player1_choice)
     player2 = Player("Player 2", player2_choice)
     result = Game.play_game(player1, player2)
-    return render_template('result.html', title='Result', result=result)
+    return render_template('result.html', title='Result', player1="Player1", player2="Player2", player1_choice=player1_choice, player2_choice=player2_choice, result=result)
