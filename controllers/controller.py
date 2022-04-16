c2deb84dd88d391c6c212c2ce80391b111d4a3bd
emma-dougherty <doughertyemma@hotmail.com>
2022-04-16 11:13:24 +0100

    third commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.player import Player
 from models.game import Game
@@ -7,9 +7,14 @@ from models.game import Game
 def home ():
     return render_template('index.html', title='Play!')
 
-@app.route('/<player1_choice>/<player2_choice>')
+@app.route('/<player1_choice>/<player2_choice>', methods=['POST'])
 def play(player1_choice, player2_choice):
-    player1 = Player("Player 1", player1_choice)
-    player2 = Player("Player 2", player2_choice)
+    player1_name = request.form["name1"]
+    player2_name = request.form["name2"]
+    player1_choice = request.form["choice1"]
+    player2_choice = request.form["choice2"]
+    player1 = Player(player1_name, player1_choice)
+    player2 = Player(player2_name, player2_choice)
     result = Game.play_game(player1, player2)
-    return render_template('result.html', title='Result', player1="Player1", player2="Player2", player1_choice=player1_choice, player2_choice=player2_choice, result=result)
+    return render_template('result.html', title='Result', player1=player1_name, player2=player2_name, player1_choice=player1_choice, player2_choice=player2_choice, result=result)
+
--- models/game.py
+++ models/game.py
@@ -1,21 +1,41 @@
 from models.player import *
 
+from random import *
 class Game():
 
     def play_game(player1, player2):
         if player1.choice == "rock" and player2.choice == "scissors":
-            return " Player1 wins by playing " + player1.choice
+            return " Player 1 wins by playing " + player1.choice
         
         if player1.choice == "scissors" and player2.choice == "paper":
-            return " Player1 wins by playing " + player1.choice
+            return " Player 1 wins by playing " + player1.choice
         
         if player1.choice == "paper" and player2.choice == "rock":
-            return " Player1 wins by playing " + player1.choice
+            return " Player 1 wins by playing " + player1.choice
         
         if player1.choice == player2.choice:
             return None
 
-        return " Player2 wins by playing " + player2.choice
+        return " Player 2 wins by playing " + player2.choice
+
+    def play_computer(human):
+        computer = "Computer"
+        computer.choice = random.choice("rock","paper","scissors")
+
+        if human.choice == "rock" and computer.choice == "scissors":
+            return " Player 1 wins by playing " + human.choice
+        
+        if human.choice == "scissors" and computer.choice == "paper":
+            return " Player 1 wins by playing " + human.choice
+        
+        if human.choice == "paper" and computer.choice == "rock":
+            return " Player 1 wins by playing " + human.choice
+        
+        if human.choice == computer.choice:
+            return None
+
+        return "Computer wins by playing " + computer.choice
+
 
 
 
