66ce99818ad59815d7bc6b85dc760608d0ee951e
emma-dougherty <doughertyemma@hotmail.com>
2022-04-17 22:54:23 +0100

    forth commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,19 +2,43 @@ from flask import render_template, request
 from app import app
 from models.player import Player
 from models.game import Game
+import random
 
 @app.route('/')
 def home ():
     return render_template('index.html', title='Play!')
 
-@app.route('/<player1_choice>/<player2_choice>', methods=['POST'])
-def play(player1_choice, player2_choice):
-    player1_name = request.form["name1"]
-    player2_name = request.form["name2"]
-    player1_choice = request.form["choice1"]
-    player2_choice = request.form["choice2"]
-    player1 = Player(player1_name, player1_choice)
-    player2 = Player(player2_name, player2_choice)
-    result = Game.play_game(player1, player2)
-    return render_template('result.html', title='Result', player1=player1_name, player2=player2_name, player1_choice=player1_choice, player2_choice=player2_choice, result=result)
+@app.route('/play')
+def playc ():
+    return render_template('playcomputer.html', title='Play Computer!')
+
+@app.route('/test')
+def test ():
+    return render_template('test.html', title='Test')
+
+### This works with index.html for two players, but with play against computer endpoint, it causes problems. Have not been able to work out why - has to do with ["name1"] ###
+
+# @app.route('/<player1_choice>/<player2_choice>', methods=['POST'])
+# def play(player1_choice, player2_choice):
+#     player1_name = request.form["name1"]
+#     player2_name = request.form["name2"]
+#     player1_choice = request.form["choice1"]
+#     player2_choice = request.form["choice2"]
+#     player1 = Player(player1_name, player1_choice)
+#     player2 = Player(player2_name, player2_choice)
+#     result = Game.play_game(player1, player2)
+#     return render_template('result.html', title='Result', player1=player1_name, player2=player2_name, player1_choice=player1_choice, player2_choice=player2_choice, result=result)
+
+@app.route('/<player_choice>/<computer_choice>', methods=['POST'])
+def play_computer(player_choice, computer_choice):
+    player_name = request.form["name"]
+    player2_name = "Computer"
+    computer = Player("Computer", computer_choice)
+    computer.choice = random.choice(computer_choice)
+    player_choice = request.form["choice"]
+    player1 = Player(player_name, player_choice)
+    player2 = Player(player2_name, computer_choice)
+    result = Game.play_computer(player1, player2)
+    return render_template('result.html', title='Result', player1=player_name, player2="Computer", player1_choice=player_choice, player2_choice=computer_choice, result=result)
+
 
--- models/game.py
+++ models/game.py
@@ -1,40 +1,41 @@
+import random
 from models.player import *
 
-from random import *
 class Game():
 
     def play_game(player1, player2):
         if player1.choice == "rock" and player2.choice == "scissors":
-            return " Player 1 wins by playing " + player1.choice
+            return f"Player 1 wins by playing {player1.player_choice}"
         
         if player1.choice == "scissors" and player2.choice == "paper":
-            return " Player 1 wins by playing " + player1.choice
+            return f"Player 1 wins by playing {player1.choice}"
         
         if player1.choice == "paper" and player2.choice == "rock":
-            return " Player 1 wins by playing " + player1.choice
+            return f"Player 1 wins by playing {player1.choice}"
         
         if player1.choice == player2.choice:
             return None
 
         return " Player 2 wins by playing " + player2.choice
 
-    def play_computer(human):
-        computer = "Computer"
-        computer.choice = random.choice("rock","paper","scissors")
+    def play_computer(human, computer):
+        computer_choice = ["rock","paper","scissors"]
+        computer = Player("Computer", computer_choice)
+        computer.choice = random.choice(computer_choice)
 
         if human.choice == "rock" and computer.choice == "scissors":
-            return " Player 1 wins by playing " + human.choice
+            return f"Player 1 wins by playing {human.choice}"
         
         if human.choice == "scissors" and computer.choice == "paper":
-            return " Player 1 wins by playing " + human.choice
+            return f"Player 1 wins by playing {human.choice}"
         
         if human.choice == "paper" and computer.choice == "rock":
-            return " Player 1 wins by playing " + human.choice
+            return f"Player 1 wins by playing {human.choice}"
         
         if human.choice == computer.choice:
             return None
 
-        return "Computer wins by playing " + computer.choice
+        return f"Computer wins by playing {computer.choice}"
 
 
 
